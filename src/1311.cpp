b204be7f2c76911b88e9e74d9f32f468ff01ae6f
wuyi <jiejielyu@outlook.com>
2020-02-19 18:26:02 +0800

    doc(1311.cpp): add examples

--- src/1311.cpp
+++ src/1311.cpp
@@ -17,6 +17,23 @@
 (g) 0 <= id < n
 (h) 1 <= level < n
 (i) 如果friends[i] 包含 j, 则 friends[j] 包含 i
+
+例1:
+Input: watchedVideos = [["A","B"],["C"],["B","C"],["D"]], friends = [[1,2],[0,3],[0,3],[1,2]], id = 0, level = 1
+Output: ["B","C"] 
+Explanation: 
+You have id = 0 (green color in the figure) and your friends are (yellow color in the figure):
+Person with id = 1 -> watchedVideos = ["C"] 
+Person with id = 2 -> watchedVideos = ["B","C"] 
+The frequencies of watchedVideos by your friends are: 
+B -> 1 
+C -> 2
+
+例2:
+Input: watchedVideos = [["A","B"],["C"],["B","C"],["D"]], friends = [[1,2],[0,3],[0,3],[1,2]], id = 0, level = 2
+Output: ["D"]
+Explanation: 
+You have id = 0 (green color in the figure) and the only friend of your friends is the person with id = 3 (yellow color in the figure).
 */
 #include <iostream>
 #include <vector>
