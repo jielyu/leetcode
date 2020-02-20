4953f4d503521170cdf350dcc571e42b362543c1
wuyi <jiejielyu@outlook.com>
2020-02-20 17:51:20 +0800

    feat(779): add problem and solution

---
+++ src/0779.cpp
@@ -0,0 +1,54 @@
+/*
+#面试刷题# 第0041期
+#Leetcode# Q779 语法的第K个符号
+在第一行上，我们写为0。现在，在每个随后的行中，我们查看前一行，
+并用01替换每个出现的0，并用10替换每个出现的1。
+给定第N行和索引K，返回第N行的第K个索引符号。（K的值为1索引。）（1个索引）。
+约束条件：
+N 是 [1, 30]范围的整数.
+K 是 [1, 2^(N-1)] 范围的整数.
+
+例1:
+Input: N = 1, K = 1
+Output: 0
+
+例2:
+Input: N = 2, K = 1
+Output: 0
+
+例3:
+Input: N = 2, K = 2
+Output: 1
+
+例4：
+Input: N = 4, K = 5
+Output: 1
+
+Explanation:
+row 1: 0
+row 2: 01
+row 3: 0110
+row 4: 01101001
+*/
+
+class Solution {
+public:
+    int get_kth_bit(const int & low, const int & high, const int & value, const int & K) {
+        if (low == high) {
+            return value;
+        }
+        int mid = (low + high) / 2;
+        int cur_val = value;
+        if (K > mid) {
+            cur_val = (value == 0 ? 1 : 0);
+            return get_kth_bit(mid + 1, high, cur_val, K);
+        }
+        return get_kth_bit(low, mid, cur_val, K);
+    }
+    
+    // Runtime: 4 ms, faster than 55.34%
+    // Memory Usage: 8.4 MB, less than 41.67%
+    int kthGrammar(int N, int K) {
+        return get_kth_bit(1, 1<<(N-1), 0, K);
+    }
+};
